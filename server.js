8a9050292cc679953a82fc986b61ab2534f00518
Sam Cook <samryancook@gmail.com>
2017-12-04 13:05:36 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+console.log(__dirname)
+
+app.use(express.static('build'))
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/build/index.html'))
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
